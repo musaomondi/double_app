5e985b0e547c660f7dccf283d447f51f1de967bd
musaomondi <amokem@yahoo.com>
2016-06-14 12:29:34 +0300

    generate static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def Home
+  end
+
+  def Contacts
+  end
+
+  def About
+  end
+
+  def Help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/Home'
+
+  get 'static_pages/Contacts'
+
+  get 'static_pages/About'
+
+  get 'static_pages/Help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
